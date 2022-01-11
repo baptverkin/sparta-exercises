d6860bd5dd9298e136a46880d869e3f97ee8fa9e
Baptiste Verkindere <bverkindere@gmail.com>
2022-01-11 14:36:00 +0100

    add exercise play with strings

--- 02_programming/01_variables/969830e3c3dda891a6b1aeed2709911232022bb1/01_play_with_strings/src/index.js
+++ 02_programming/01_variables/969830e3c3dda891a6b1aeed2709911232022bb1/01_play_with_strings/src/index.js
@@ -1,4 +1,7 @@
 // a constant called `question`, bound to a string: "Spartan, what is your profession?!";
-
+const question = "Spartan, what is your profession?!";
+console.log(question)
 
 // a constant called `answer`, bound to a string: "Dev dev dev!";
+const answer = "Dev dev dev!";
+console.log(answer)

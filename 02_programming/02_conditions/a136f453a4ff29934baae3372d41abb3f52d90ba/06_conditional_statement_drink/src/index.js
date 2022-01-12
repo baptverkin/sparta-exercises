5c562f5db8ce5f0cfd5ea85070765ec1e6e1c80f
Baptiste Verkindere <bverkindere@gmail.com>
2022-01-12 14:33:08 +0100

    answers to exercise 6

--- 02_programming/02_conditions/a136f453a4ff29934baae3372d41abb3f52d90ba/06_conditional_statement_drink/src/index.js
+++ 02_programming/02_conditions/a136f453a4ff29934baae3372d41abb3f52d90ba/06_conditional_statement_drink/src/index.js
@@ -2,8 +2,19 @@
 
 // Create two objects, `frieda`, `francis` with the fields :
 //   `age` (an integer) and `name`, (obviously a string)
-
-
+const frieda = {
+  age: 22,
+  name: "Frieda",
+};
+const francis = {
+  age: 17,
+  name: "Francis",
+};
 
 // Create two variables `canFriedaDrinkAlcohol` and `canFrancisDrinkAlcohol` to answer,
 // respectively, to the question "Can he / she drink alcohol?".
+const canFriedaDrinkAlcohol = frieda.age >= 18 ? true : false;
+const canFrancisDrinkAlcohol = francis.age >= 18 ? true : false;
+
+console.log(canFriedaDrinkAlcohol);
+console.log(canFrancisDrinkAlcohol);

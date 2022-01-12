de009eadb39cb9163ac428ef353fceed3f5398cb
Baptiste Verkindere <bverkindere@gmail.com>
2022-01-12 13:52:29 +0100

    andding answers to exercise 5bis

--- 02_programming/02_conditions/a136f453a4ff29934baae3372d41abb3f52d90ba/05_even_and_odd_numbers_ternary/src/index.js
+++ 02_programming/02_conditions/a136f453a4ff29934baae3372d41abb3f52d90ba/05_even_and_odd_numbers_ternary/src/index.js
@@ -1,3 +1,4 @@
 let numberToCheck = 18;
 
 // transform (or better, re do) the code of your previous exercise using a ternary operator.
+console.log(numberToCheck%2 === 0 ?  "The number is even" : "The number is odd");

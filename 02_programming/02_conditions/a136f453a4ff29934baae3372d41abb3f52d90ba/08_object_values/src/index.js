0664dac6ed7878737c22977e5a6761ddd4e57089
Baptiste Verkindere <bverkindere@gmail.com>
2022-01-12 15:37:54 +0100

    adding changes of ESlint parameters

--- 02_programming/02_conditions/a136f453a4ff29934baae3372d41abb3f52d90ba/08_object_values/src/index.js
+++ 02_programming/02_conditions/a136f453a4ff29934baae3372d41abb3f52d90ba/08_object_values/src/index.js
@@ -1,6 +1,6 @@
 const armoury = {
   spear: "Gungnir",
-  hammer: "Mjöllnir",
+  hammer: "Mjöllnir",     
   chain: "Gleipnir",
   sword: "Hofund",
   shoes: "Vidar's shoes",
@@ -13,12 +13,9 @@ const odinRequest = "Mjöllnir";
 // using the object method to get all values (google is your friend)
 const armouryWithNamesOnly = Object.values(armoury);
 console.log(armouryWithNamesOnly);
-
-
 // Create a constant isTheRequestInArmoury bound to the check of the presence
 // of odinRequest in armouryWithNamesOnly
 const isTheRequestInArmoury = armouryWithNamesOnly.includes(odinRequest) ? true : false;
-
 
 // Create a condition that logs "Ah, I finally found something to hammer that nail into"
 // if the isTheRequestInArmoury is true and "What do you want me to do with this?" if not

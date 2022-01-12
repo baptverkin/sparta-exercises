97f42c9f92907b37af9e0eb8831155877bc3a2e1
Baptiste Verkindere <bverkindere@gmail.com>
2022-01-12 12:07:43 +0100

    adding answers to exercise 3

--- 02_programming/02_conditions/a136f453a4ff29934baae3372d41abb3f52d90ba/03_includes_array/src/index.js
+++ 02_programming/02_conditions/a136f453a4ff29934baae3372d41abb3f52d90ba/03_includes_array/src/index.js
@@ -1,15 +1,17 @@
 const scientistsNumbers = [666, 3141, 23, 1618, 42];
 
 // Create a constant theComputedOne that is bound to the number 42
-
+const theComputedOne = 42;
+console.log(theComputedOne);
 
 
 // Create a constant called isTheComputedOneInOurNumbers bound to the value
 // of includes used on the array someRandomNumbers with the variable theComputedOne
-
+const isTheComputedOneInOurNumbers = scientistsNumbers.includes(theComputedOne);
+console.log(isTheComputedOneInOurNumbers);
 
 
 // Create a condition that will log "The Answer to the Ultimate Question of Life, the Universe, and Everything is " and
 // the variable theComputedOne if the number theComputedOne is in the array someRandomNumbers then ", I knew it!",
 // "I always thought something was fundamentally wrong with the universe." if not
-
+console.log(isTheComputedOneInOurNumbers ? `The Answer to the Ultimate Question of Life, the Universe, and Everything is ${theComputedOne}, I knew it!` : "I always thought something was fundamentally wrong with the universe.");

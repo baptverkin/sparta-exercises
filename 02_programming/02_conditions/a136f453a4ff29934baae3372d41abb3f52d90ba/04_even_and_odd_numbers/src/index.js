7ee0209d749d878a6ebd0f14ee196dd566c32444
Baptiste Verkindere <bverkindere@gmail.com>
2022-01-12 12:26:00 +0100

    adding answers to exercise 5

--- 02_programming/02_conditions/a136f453a4ff29934baae3372d41abb3f52d90ba/04_even_and_odd_numbers/src/index.js
+++ 02_programming/02_conditions/a136f453a4ff29934baae3372d41abb3f52d90ba/04_even_and_odd_numbers/src/index.js
@@ -2,3 +2,7 @@ let numberToCheck = 18;
 
 // Code here a conditional statement that will log "The number is even" when
 // numberToCheck is even and "The number is odd" when it is odd
+let remainderNumberToCheck = numberToCheck%2;
+console.log(remainderNumberToCheck);
+
+console.log(remainderNumberToCheck === 0 ? "The number is even" : "The number is odd");

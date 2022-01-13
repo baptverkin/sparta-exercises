d0eb1e21b3bebdf7bd20be85ee6ec152c60c280b
Baptiste Verkindere <bverkindere@gmail.com>
2022-01-13 14:49:29 +0100

    adding answers to exercise 3 - day 3

--- 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/03_odd_even/src/index.js
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/03_odd_even/src/index.js
@@ -1,6 +1,14 @@
-function oddEven() {
+function oddEven(number) {
   // Code the function here.
+  if (Number.isInteger(number)) {
+    return number%2 === 0 ? `${number} is an even number.` : `${number} is an odd number.`;
+  } else {
+    return console.log(`${number} is not a number.`);
+  }
 }
+
+console.log(oddEven("two"));
+
 
 // Do not remove last lines, it is for tests
 module.exports = oddEven;

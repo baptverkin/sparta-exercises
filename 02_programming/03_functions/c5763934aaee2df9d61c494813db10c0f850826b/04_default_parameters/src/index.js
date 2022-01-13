fc23ad86536cfc54c18d55a43594f41941df0aed
Baptiste Verkindere <bverkindere@gmail.com>
2022-01-13 15:33:03 +0100

    adding answers to exercise 4 day 3

--- 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/04_default_parameters/src/index.js
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/04_default_parameters/src/index.js
@@ -1,6 +1,10 @@
-function helloSpartan() {
+function helloSpartan(argument = "soldier") {
   // Code the function here.
+  console.log(argument === "soldier" ? `Hello ${argument}; not very talkative uh?` : `Hello ${argument}: glad to know your name!`);
 }
+
+helloSpartan("soldier");
+
 
 // Do not remove last lines, it is for tests
 module.exports = helloSpartan;

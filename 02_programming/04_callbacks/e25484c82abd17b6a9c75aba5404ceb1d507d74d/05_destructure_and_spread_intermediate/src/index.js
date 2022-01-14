f63e0695010dfcc0ca907f8e7160b0e289af4631
Baptiste Verkindere <bverkindere@gmail.com>
2022-01-14 15:20:19 +0100

    adding answers to exercise 5

--- 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/05_destructure_and_spread_intermediate/src/index.js
+++ 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/05_destructure_and_spread_intermediate/src/index.js
@@ -10,12 +10,14 @@ let storageHell = [
 ];
 // Should be cleaned this way => ['IPad', 'IPhone', 'GameBoy color', 'Nes', 'Donkey kong 64', 'Apex Legends Starter Pack', 'LG 5K 27p screen', 'Coffee Machine', 'Azelad']
 
-function formatStorage() {
+function formatStorage(array) {
   // Code your function here
+  return [...array[1], ...array[2], ...array[3], ...array[4], ...array[5]];
 }
 
+
 // ↓ Feel free to uncomment this next line, or even create more to try everything you do ↓
-// console.log(formatStorage(storageHell));
+console.log(formatStorage(storageHell));
 
 // Don't modify this, it is for the tests.
 module.exports = formatStorage;

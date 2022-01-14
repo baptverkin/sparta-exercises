924c0b013a9bd39d4ac87b504e79104488d97670
Baptiste Verkindere <bverkindere@gmail.com>
2022-01-14 12:12:32 +0100

    answers to exercise 2

--- 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/02_use_callback/src/getInput.js
+++ 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/02_use_callback/src/getInput.js
@@ -1,5 +1,7 @@
 // A function getInput that takes two parameters, an input (the first one) and a callback function
+const getInput = ({user, key}, callback) => callback(key);
 
-const getInput = // code the function here
-  // Leave the line below for tests to work
-  (module.exports = getInput);
+
+// code the function here
+// Leave the line below for tests to work
+module.exports = getInput;

085cf606e8d6808f44d5aafea4dc5260688471a6
Baptiste Verkindere <bverkindere@gmail.com>
2022-01-14 16:37:21 +0100

    adding answers to exercise 6

--- 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/06_recursion/src/recursion.js
+++ 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/06_recursion/src/recursion.js
@@ -1,6 +1,16 @@
-const findPosition = () => {
-  // Code here
+const findPosition = (array, string, integer) => {
+
+  if ((Array.isArray(array)) && (typeof array[0] === "string") && (typeof string === "string") && (Number.isInteger(integer))) {
+    if (array.includes(string)){
+      return `${string} is at position #${array.indexOf(string)+1} in this array.`;
+    } else {
+      return `${string} is not present in this array.`;
+    };
+  } else {
+    throw new Error ("Invalid parameters");
+  };
 };
+
 
 // Leave line below for tests to work properly
 module.exports = findPosition;

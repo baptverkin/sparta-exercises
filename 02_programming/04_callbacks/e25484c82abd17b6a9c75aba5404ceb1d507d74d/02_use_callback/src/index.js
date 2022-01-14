d22bdf676a31f5a4a331e5d802978638026a0efa
Baptiste Verkindere <bverkindere@gmail.com>
2022-01-14 14:25:54 +0100

    adding answers to exercise 3

--- 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/02_use_callback/src/index.js
+++ 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/02_use_callback/src/index.js
@@ -5,3 +5,4 @@ const getInput = require("./getInput");
 const log = (text) => console.log(text);
 
 getInput({user: "John", key: "arrow_up"}, log);
+
--- 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/03_choose_operation/src/chooseOperation.js
+++ 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/03_choose_operation/src/chooseOperation.js
@@ -1,21 +1,25 @@
-function addition() {
-  // Code here
-}
+function addition(number1, number2) {
+  return number1 + number2;
+};
 
-function substraction() {
-  // Code here
-}
+function substraction(number1, number2) {
+  return number1 - number2;
+};
 
-function multiplication() {
-  // Code here
-}
+function multiplication(number1, number2) {
+  return number1 * number2;
+};
 
-function division() {
-  // Code here
-}
+function division(number1, number2) {
+  return number1 / number2;
+};
 
-function chooseOperation() {
-  // Code here
+function chooseOperation(number1, number2, callback) {
+  if ((Number.isInteger(number1) && Number.isInteger (number2) && (typeof callback === "function"))){
+    return callback(number1, number2);
+  } else {
+    throw new Error("Parameters are not correct");
+  };
 }
 
 // Leave the line below for tests to work

a94b7ca67ea681e03b42a08e1a78b27951df79e3
Baptiste Verkindere <bverkindere@gmail.com>
2022-01-17 13:49:56 +0100

    adding aswers to exercise 3 day 5

--- 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/03_every_frame_a_painting/src/index.js
+++ 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/03_every_frame_a_painting/src/index.js
@@ -1,6 +1,13 @@
 function canTakeArtPieces(pieces) {
-
+  return pieces.every(pieces => pieces.type !== "painting" ? false : true );
 }
+
+/*
+const pieces = [
+  {piece: "Mona Lisa", type: "other"},
+  {piece: "Guernica", type: "painting"}
+];*/
+
 
 // Do not remove the following line, it is for tests
 module.exports = canTakeArtPieces;

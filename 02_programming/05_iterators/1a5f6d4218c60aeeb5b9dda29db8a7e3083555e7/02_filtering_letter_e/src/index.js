78001f24695e558b4d7c8c074227cc9180cfa53b
Baptiste Verkindere <bverkindere@gmail.com>
2022-01-17 11:51:08 +0100

    answers to exercise 2 day 5

--- 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/02_filtering_letter_e/src/index.js
+++ 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/02_filtering_letter_e/src/index.js
@@ -1,6 +1,9 @@
 function theEFilter(sentences) {
-
+  return sentences.filter(sentences => sentences.match("e") || sentences.match("E"));
 }
+
+//const newArray = sentencesToBeTested.filter(theEFilter);
+//console.log(newArray);
 
 // Do not remove the following line, it is for tests
 module.exports = theEFilter;

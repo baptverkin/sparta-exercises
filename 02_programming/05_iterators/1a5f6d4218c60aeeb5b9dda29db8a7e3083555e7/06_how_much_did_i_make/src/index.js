60cb141255830d42de4ad93e9fa7e4e497b339ea
Baptiste Verkindere <bverkindere@gmail.com>
2022-01-17 15:42:34 +0100

    adding answers to exercise 6 day 5

--- 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/06_how_much_did_i_make/src/index.js
+++ 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/06_how_much_did_i_make/src/index.js
@@ -1,12 +1,21 @@
 function howMuchDidIMake(invoices) {
-  // Code here
+  const iAmTheWaiter = invoices.filter(invoices => invoices.waiter === "Yourself");
+  const tips = iAmTheWaiter.map(tips => tips.tip);
+  const sumOfTips = tips.reduce((a, b) => a + b, 0);
+  console.log(tips);
+  return sumOfTips;
 }
+
 
 const invoices = [
   { clients: 3, waiter: "Yourself", price: 5100, tip: 200 },
   { clients: 2, waiter: "Dominique", price: 2200, tip: 50 },
   { clients: 2, waiter: "Yourself", price: 2900, tip: 100 },
 ];
+
+const result = howMuchDidIMake(invoices);
+console.log(result);
+
 
 // Do not remove the following line, it is for tests
 module.exports = howMuchDidIMake;

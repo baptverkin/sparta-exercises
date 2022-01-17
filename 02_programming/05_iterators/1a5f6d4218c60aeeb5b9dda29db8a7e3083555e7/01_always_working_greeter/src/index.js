98e5554f05522e44ddace3055a441f1f750bbfa8
Baptiste Verkindere <bverkindere@gmail.com>
2022-01-17 10:57:30 +0100

    adding files for 5th day and answers to execise 1

---
+++ 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/01_always_working_greeter/src/index.js
@@ -0,0 +1,12 @@
+function greeter(listOfPeople) {
+  listOfPeople.forEach((persons) => console.log(`Hello ${persons}!`));
+}
+//const listOfPeople = ["max", "Ben", "Bapt"];
+//const result = greeter(listOfPeople);
+
+// console.log(result);
+
+
+
+// Do not remove the following lines, it is for tests
+module.exports = greeter;

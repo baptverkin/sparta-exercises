20594ff6583f25b1503ea19e2bfe6391ca5b18a6
Baptiste Verkindere <bverkindere@gmail.com>
2022-01-17 15:28:43 +0100

    adding answers to exercise 5 day 5

--- 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/05_only_my_tips/src/index.js
+++ 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/05_only_my_tips/src/index.js
@@ -1,5 +1,7 @@
 function onlyMyTips(invoices) {
-  // Code here
+  const iAmTheWaiter = invoices.filter(invoices => invoices.waiter === "Yourself");
+  const tips = iAmTheWaiter.map(tips => tips.tip);
+  return tips;
 }
 
 const todaysInvoices = [
@@ -7,6 +9,11 @@ const todaysInvoices = [
   { clients: 2, waiter: "Dominique", price: 2200, tip: 50 },
   { clients: 2, waiter: "Yourself", price: 2900, tip: 100 },
 ];
+
+const result = onlyMyTips(todaysInvoices);
+console.log(result);
+
+//.map(invoices => invoices.slice("tip:"));
 
 // Do not remove the following line, it is for tests
 module.exports = onlyMyTips;

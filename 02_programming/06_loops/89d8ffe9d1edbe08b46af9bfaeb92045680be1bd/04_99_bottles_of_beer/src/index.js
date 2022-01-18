ab856b2bdd2c0d897bbf4e008e7126fb04f0c34f
Baptiste Verkindere <bverkindere@gmail.com>
2022-01-18 13:42:43 +0100

    answers to exercise 4 day 6

--- 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/04_99_bottles_of_beer/src/index.js
+++ 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/04_99_bottles_of_beer/src/index.js
@@ -1,6 +1,29 @@
-function sing () {
-  // code here
-};
+const minusOne = (number) => number - 1;
+// console.log (minusOne(99));
+
+function sing (numberOfBeersLeft=99) {
+  let newArray = [];
+  for (let i=numberOfBeersLeft ; i>2 ; i--){
+    newArray.push(`${i} bottles of beer on the wall, ${i} bottles of beer.`);
+    newArray.push(`Take one down and pass it around, ${(i-1)} bottles of beer on the wall.`);
+  }
+  for (let i=2 ; i>1 ; i--){
+    newArray.push(`${i} bottles of beer on the wall, ${i} bottles of beer.`);
+    newArray.push(`Take one down and pass it around, ${(i-1)} bottle of beer on the wall.`);
+  }
+  for (let i=1 ; i>0 ; i--){
+    newArray.push(`${i} bottle of beer on the wall, ${i} bottle of beer.`);
+    newArray.push("Take one down and pass it around, no more bottles of beer on the wall.");
+  }
+  for (let i=0 ; i>=0 ; i--){
+    newArray.push("No more bottles of beer on the wall, no more bottles of beer.");
+    newArray.push(`Go to the store and buy some more, ${numberOfBeersLeft} bottles of beer on the wall.`);
+  }
+  return newArray;
+}
+
+console.log(sing(99, minusOne).length);
+
 
 // Do not remove the following line, it is for tests
 module.exports = sing;

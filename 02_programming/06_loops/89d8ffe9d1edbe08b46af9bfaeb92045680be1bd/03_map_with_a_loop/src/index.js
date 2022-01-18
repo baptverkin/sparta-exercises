c13ee8a221f5f5d5d39f10267247de2ee12aa19c
Baptiste Verkindere <bverkindere@gmail.com>
2022-01-18 11:41:48 +0100

    adding aswers to exercise 3 day 6

--- 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/03_map_with_a_loop/src/index.js
+++ 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/03_map_with_a_loop/src/index.js
@@ -1,7 +1,19 @@
-function mapLoop() {
-  // Code the function here.
 
+const double = (numbers) => numbers * 2;
+const numbers = [1, 2, 3, 4];
+console.log(double(numbers));
+
+
+
+function mapLoop(numbers, double) {
+  let newNumbers = [];
+  for (let i = 0 ; i < numbers.length ; i++) {
+    newNumbers.push(double(numbers[i]));
+  }
+  return newNumbers;
 }
+
+
 
 // Do not remove the following lines, it is for tests
 module.exports = mapLoop;

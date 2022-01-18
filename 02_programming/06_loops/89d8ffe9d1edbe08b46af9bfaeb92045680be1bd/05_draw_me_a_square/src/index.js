96c969e143a10a8f8f3dadc0be8ee71053d9019d
Baptiste Verkindere <bverkindere@gmail.com>
2022-01-18 14:46:20 +0100

    adding aswers to exercise 5

--- 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/05_draw_me_a_square/src/index.js
+++ 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/05_draw_me_a_square/src/index.js
@@ -1,6 +1,23 @@
-function square() {
-  // Code here
+
+function square(number) {
+  let string = "*";
+  for(let i = 1 ; i <= number ; i++){
+    console.log(string.repeat(number));}
+  return string;
 }
+
+console.log(square(10));
+
+// function square(number) {
+//   let string = "*";
+//   for (let i = 1; i <= number; i++){ // iterates rows
+//     for (let j = 1; j <= number; j++) // iterates * in row
+//       console.log(string.repeat(i*(number-i+1)));
+//   }
+//   console.log("\n"); // moves to new row
+// }
+
+// console.log(square(10));
 
 // Do not remove the following line, it is for tests
 module.exports = square;

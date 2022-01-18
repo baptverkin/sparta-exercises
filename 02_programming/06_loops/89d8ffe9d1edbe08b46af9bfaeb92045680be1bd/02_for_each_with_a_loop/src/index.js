4c413450f46597a10d40ac6e3ca528834508e967
Baptiste Verkindere <bverkindere@gmail.com>
2022-01-18 10:46:03 +0100

    adding answers to exercise 2 day 6

--- 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/02_for_each_with_a_loop/src/index.js
+++ 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/02_for_each_with_a_loop/src/index.js
@@ -1,7 +1,11 @@
-function forEachLoop() {
-  // Code the function here.
+const log = (array) => (`* ${array}`);
 
+function forEachLoop(array, log) {
+  for (let i=0 ; i < array.length ; i++) {
+    log(array[i]);
+  }
 }
+
 
 // Do not remove the following lines, it is for tests
 module.exports = forEachLoop;

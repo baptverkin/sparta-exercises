c6ae1493ccf34fdaba211164e682ceae69711064
Baptiste Verkindere <bverkindere@gmail.com>
2022-01-18 14:49:05 +0100

    adding awsers to exercise 6 day 6

--- 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/06_draw_me_a_triangle/src/index.js
+++ 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/06_draw_me_a_triangle/src/index.js
@@ -1,6 +1,11 @@
-function triangle() {
-  // Code here
+function triangle(number) {
+  let string = "*";
+  for(let i = 1 ; i <= number ; i++){
+    console.log(string.repeat(i));}
+  return string;
 }
+
+console.log(triangle(10));
 
 // Do not remove the following line, it is for tests
 module.exports = triangle;

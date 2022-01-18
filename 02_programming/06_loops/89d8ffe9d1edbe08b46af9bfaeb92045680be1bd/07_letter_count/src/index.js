7e7c20209f8ccea75f2675e03a204fc2d0754d8f
Baptiste Verkindere <bverkindere@gmail.com>
2022-01-18 15:01:26 +0100

    adding answers to exercise 7 day 6

--- 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/07_letter_count/src/index.js
+++ 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/07_letter_count/src/index.js
@@ -1,6 +1,7 @@
-function letterCount() {
-  // Code here
+function letterCount(stringToTest, characterToTest) {
+  return stringToTest.split(characterToTest).length-1;
 }
+letterCount("gattaca", "z");
 
 // Do not remove the following line, it is for tests
 module.exports = letterCount;

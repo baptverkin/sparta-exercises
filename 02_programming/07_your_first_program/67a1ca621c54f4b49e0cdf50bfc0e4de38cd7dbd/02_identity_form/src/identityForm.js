465b1093d6f0e6f2c675ab0c872a00fd5b12c50d
Baptiste Verkindere <bverkindere@gmail.com>
2022-01-19 10:22:08 +0100

    adding anwsers to exercise 2

--- 02_programming/07_your_first_program/67a1ca621c54f4b49e0cdf50bfc0e4de38cd7dbd/02_identity_form/src/identityForm.js
+++ 02_programming/07_your_first_program/67a1ca621c54f4b49e0cdf50bfc0e4de38cd7dbd/02_identity_form/src/identityForm.js
@@ -1,5 +1,12 @@
 const identityForm = (reader) => {
-  // Code here
+  reader.question("Hello ! What's your first name? ", (firstName) => {
+    reader.question("What's your last name? ", (lastName) => {
+      reader.question("What's your age? ", (age) => {
+        console.log(`Your name is ${firstName} ${lastName} and you are ${age}.`);
+        reader.close();
+      });
+    });
+  });
 };
 
 // Leave line below for tests to work

0aa2b04a8df06f9a6d6e8cd447e67bd0d64fb45e
Baptiste Verkindere <bverkindere@gmail.com>
2022-01-19 15:08:44 +0100

    adding answers to exercise 3

--- 02_programming/07_your_first_program/67a1ca621c54f4b49e0cdf50bfc0e4de38cd7dbd/03_number_game/src/numberGame.js
+++ 02_programming/07_your_first_program/67a1ca621c54f4b49e0cdf50bfc0e4de38cd7dbd/03_number_game/src/numberGame.js
@@ -1,5 +1,49 @@
+
+// function getRandomNumber(min, max) {
+//   min = Math.ceil(min);
+//   max = Math.floor(max);
+//   return Math.floor(Math.random() * (max - min)+1) + min;
+// };
+// const randomNumber = getRandomNumber(1, 100);
+
+
+
 function numberGame(reader, min = 1, max = 100) {
-  // code here
+  const randomNumber = Math.round(Math.random() * (max - min)) + min;
+  console.log(randomNumber);
+
+
+  console.log("Welcome!\nYou have to find the right number, between 1 and 100!\nGood luck!!");
+
+  reader.question ("\nEnter a number\n>", callback);
+  function callback (number){
+    const makeNumberANumber = Number(number);
+
+    if (Number.isInteger(makeNumberANumber)){
+      if (makeNumberANumber === randomNumber){
+        console.log("You won!");
+      } else if(makeNumberANumber<min || makeNumberANumber>max){
+        console.log("The number is between 1 and 100");
+        return reader.question("\nEnter a number\n", callback);
+      }else if (makeNumberANumber>randomNumber){
+        console.log("Too high");
+        return reader.question("\nEnter a number\n", callback);
+      }else if (makeNumberANumber<randomNumber){
+        console.log("Too low");
+        return reader.question("\nEnter a number\n", callback);
+      } else if (makeNumberANumber===max) {
+        console.log("You won!");
+        return reader.question("\nEnter a number\n", callback);
+      } else if (makeNumberANumber===min) {
+        console.log("You won!");
+        return reader.question("\nEnter a number\n", callback);
+      }
+    }else{
+      console.log("This was not a number");
+      return reader.question("\nEnter a number\n", callback);
+    }
+    reader.close();
+  }
 }
 
 module.exports = numberGame;

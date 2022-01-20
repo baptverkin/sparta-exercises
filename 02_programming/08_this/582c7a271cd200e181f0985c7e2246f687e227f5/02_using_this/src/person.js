bd5fa127886c2008bea062fd75fd425387c0598a
Baptiste Verkindere <bverkindere@gmail.com>
2022-01-20 10:48:52 +0100

    adding asnwers to exercise 2

--- 02_programming/08_this/582c7a271cd200e181f0985c7e2246f687e227f5/02_using_this/src/person.js
+++ 02_programming/08_this/582c7a271cd200e181f0985c7e2246f687e227f5/02_using_this/src/person.js
@@ -5,7 +5,12 @@ const person = {
   fullname: function () {
     return `${this.firstname} ${this.lastname}`;
   },
-  // Complete here
+  introduceMyself: function(){
+    return `Hello! I'm ${this.firstname} ${this.lastname} and I'm ${this.age}.`;
+  },
 };
+
+const introduction = person.introduceMyself();
+console.log(introduction); // Hello! I'm Abdel Sadki and I'm 42
 
 module.exports = person;

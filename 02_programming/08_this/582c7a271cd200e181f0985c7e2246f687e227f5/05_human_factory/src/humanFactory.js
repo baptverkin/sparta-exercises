261a6274955b135a9760f7aa5e681dcfdcb1685d
Baptiste Verkindere <bverkindere@gmail.com>
2022-01-20 18:55:51 +0100

    adding answers to exercise 5

--- 02_programming/08_this/582c7a271cd200e181f0985c7e2246f687e227f5/05_human_factory/src/humanFactory.js
+++ 02_programming/08_this/582c7a271cd200e181f0985c7e2246f687e227f5/05_human_factory/src/humanFactory.js
@@ -1,10 +1,45 @@
-const humanFactory = (humanData = {}) => {
-  // code here
+const humanFactory = (humanData) => {
+
+  const defaultMale = {
+    firstName: "John",
+    lastName: "Doe",
+    genre: "male",
+    job: "unemployed",
+    fullname : function(){
+      return `${this.firstName} ${this.lastName}`;
+    },
+    introduction: function () {
+      return `Hello! My name is ${this.firstName} ${this.lastName}.`;
+    },
+  };
+  const defaultFemale = {
+    firstName: "Jane",
+    genre: "female",
+  };
+
+  if (humanData=== undefined)
+    return defaultMale;
+  else if (Object.values(humanData).includes("female")){
+    return Object.assign(defaultMale, defaultFemale);
+  }else{
+    return Object.assign(defaultMale, humanData);
+  };
 };
 
 const createHumans = (humans) => {
   // code here
 };
+
+// const target = { a: 1, b: 5 };
+//const source = { b: 3, c: 5 };
+
+//const returnedTarget = Object.assign(target, source);
+
+// console.log(target);
+// expected output: Object { a: 1, b: 4, c: 5 }
+
+//console.log(returnedTarget);
+// expected output: Object { a: 1, b: 4, c: 5 }
 
 module.exports = {
   humanFactory,

a932298d8e18326aae5ce4defe80172bc15a1b54
Baptiste Verkindere <bverkindere@gmail.com>
2022-01-20 12:52:39 +0100

    adding answers to exercise 3

--- 02_programming/08_this/582c7a271cd200e181f0985c7e2246f687e227f5/03_chaining/src/car.js
+++ 02_programming/08_this/582c7a271cd200e181f0985c7e2246f687e227f5/03_chaining/src/car.js
@@ -1,9 +1,37 @@
 // Complete and add needed car properties:
 const car = {
   speed: 0,
+  initialDrive: 0,
+  distance: 0,
   start: function () {
-  
+    this.distance = 0;
+    return this;
+  },
+  changeSpeed: function (newSpeed) {
+    this.speed = newSpeed;
+    return this;
+  },
+  drive: function (minutesDriving) {
+    if (this.distance === 0){
+      this.initialDrive = minutesDriving;
+      this.distance = this.speed*(this.initialDrive/60);
+      console.log(this.distance);
+    }else{
+      this.initialDrive = minutesDriving;
+      this.distance = this.distance + (this.speed*(this.initialDrive/60));
+      console.log(this.distance);
+    };
+    return this;
+  },
+  // updateDistance: function (){
+  //   this.distance = this.distance + (this.speed*this.initialDrive/60);
+  //   return this;
+  // },
+  showDistance: function () {
+    console.log(`${this.distance} Km`);
+    return this;
   },
 };
+car.start().changeSpeed(130).drive(45).changeSpeed(50).drive(30).changeSpeed(90).drive(80).showDistance();
 
 module.exports = car;

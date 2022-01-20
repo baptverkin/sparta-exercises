31acf63e6e30da5473dc3bf412da7cd10e0f9f91
Baptiste Verkindere <bverkindere@gmail.com>
2022-01-20 15:46:48 +0100

    adding answers to exercise 4

--- 02_programming/08_this/582c7a271cd200e181f0985c7e2246f687e227f5/04_coffee_machine/src/coffeeMachine.js
+++ 02_programming/08_this/582c7a271cd200e181f0985c7e2246f687e227f5/04_coffee_machine/src/coffeeMachine.js
@@ -1,5 +1,30 @@
 const machine = {
-  // Complete here
+
+  litersOfCoffee: 0,
+  expressoSize: 0.08,
+  longCoffeeSize: 0.15,
+
+  fillWithLitersOfCoffee: function(recharge){
+    this.litersOfCoffee = this.litersOfCoffee + recharge;
+    return this;
+  },
+  expresso: function (){
+    if (this.litersOfCoffee >= this.expressoSize){
+      this.litersOfCoffee = this.litersOfCoffee - this.expressoSize;
+      return true;
+    }else{
+      return false;
+    };
+  },
+  longCoffee: function (){
+    if (this.litersOfCoffee >= this.longCoffeeSize){
+      console.log(this.litersOfCoffee >= this.longCoffeeSize);
+      this.litersOfCoffee = this.litersOfCoffee - this.longCoffeeSize;
+      return true;
+    }else{
+      return false;
+    }
+  },
 };
 
 module.exports = machine;

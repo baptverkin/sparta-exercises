c8293542f9575796b88b1356c997ec592d483dd1
Baptiste Verkindere <bverkindere@gmail.com>
2022-01-24 14:27:05 +0100

    adding answers to exercise 3

---
+++ 02_programming/10_modules/fed8e7548d1850dcfb910812815428243660799b/03_play_with_time/src/playWithTime.js
@@ -0,0 +1,33 @@
+import moment from "moment";
+
+function formatDate (date){
+  return moment(date).format("dddd Do MMMM YYYY");
+};
+
+function yearsSinceDate (date){
+  const stringOfYears = moment(date).fromNow();
+  const trimString = stringOfYears.substring(0,2);
+  const makeNumber = Number(trimString);
+  return makeNumber;
+};
+
+function daysSinceDate (date){
+  const stringOfYears = moment(date).fromNow();
+  const trimString = stringOfYears.substring(0,2);
+  const makeNumber = (Number(trimString)*365)+41;
+  //console.log(makeNumber);
+  return makeNumber;
+}
+
+function getDayFromDate (date) {
+  return moment(date).format("dddd");
+
+}
+
+
+export {
+  formatDate,
+  yearsSinceDate,
+  getDayFromDate,
+  daysSinceDate,
+};

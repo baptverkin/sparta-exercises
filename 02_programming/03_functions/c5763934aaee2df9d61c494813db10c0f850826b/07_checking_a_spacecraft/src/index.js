70046f80af19cfcffb53a8f4db626b451b36df50
Baptiste Verkindere <bverkindere@gmail.com>
2022-01-25 10:49:54 +0100

    adding answers to exercise 7 - Functions

--- 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/07_checking_a_spacecraft/src/index.js
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/07_checking_a_spacecraft/src/index.js
@@ -1,5 +1,36 @@
 // Code here
 
+const stealthyFuelLevelCheck = (integer) => { if(integer>20){
+  return "Fuel level: green";
+}else if (integer>10 && integer < 20){
+  return "Fuel level: yellow";
+}else{
+  return "Fuel level: red";
+}};
+
+const stealthyCargoHoldCheck = (array) => {if(array.length === 4){
+  return "Full";
+} else if (array.length < 4){
+  return `Spaces available: ${4-array.length}`;
+}else  if (array.length >4) {
+  return `Over capacity by ${array.length - 4} items.`;
+}
+};
+
+
+
+// function checkCargoHold(array){
+//   console.log(array.length);
+//   console.log(array.length === 4);
+//   if(array.length === 4){
+//     return "Full";
+//   } else if (array.length < 4){
+//     return `Spaces available: ${4-array.length}`;
+//   }else  if (array.length >4) {
+//     return `Over capacity by ${array.length - 4} items.`;
+//   }
+// }
+
 // Do not remove last lines, it is for tests
 
 module.exports = { stealthyFuelLevelCheck, stealthyCargoHoldCheck };

b0ccf8e61445c35882f7449e602cd670f4c70743
Baptiste Verkindere <bverkindere@gmail.com>
2022-01-25 10:39:37 +0100

    adding answer to exercise 6 - functions

--- 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/06_preparing_a_spacecraft/src/index.js
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/06_preparing_a_spacecraft/src/index.js
@@ -1,10 +1,26 @@
-function checkFuelLevel() {
-  // Code here
+function checkFuelLevel(integer) {
+  if(integer>20){
+    return "Fuel level: green";
+  }else if (integer>10 && integer < 20){
+    return "Fuel level: yellow";
+  }else{
+    return "Fuel level: red";
+  }
 }
 
-function checkCargoHold(){
-  // Code here
+function checkCargoHold(array){
+  console.log(array.length);
+  console.log(array.length === 4);
+  if(array.length === 4){
+    return "Full";
+  } else if (array.length < 4){
+    return `Spaces available: ${4-array.length}`;
+  }else  if (array.length >4) {
+    return `Over capacity by ${array.length - 4} items.`;
+  }
 }
+
+const result = checkCargoHold(["Burst Laser II", "Artemis Missiles", "Defense Drone Mk I", "Repair Arm"]);
 
 // Do not remove last lines, it is for tests
 module.exports = { checkFuelLevel, checkCargoHold };

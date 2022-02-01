de64d8eec763c63c1d227d9f3de873d0729f9ccd
Baptiste Verkindere <bverkindere@gmail.com>
2022-02-01 10:58:35 +0100

    adding answers to exercise 2

--- 02_programming/16_typescript_oop/bf7decead6a2841402b38470fef1130c0f7a2ef3/02_orange_tree_part_one/src/index.ts
+++ 02_programming/16_typescript_oop/bf7decead6a2841402b38470fef1130c0f7a2ef3/02_orange_tree_part_one/src/index.ts
@@ -1,4 +1,33 @@
 // Code the class here.
 
+class Tree {
+  age: number;
+  height: number;
+  alive: boolean = true;
+
+  constructor (age: number){
+    this.age = age;
+    if (age===0){
+      this.height = 0;
+    }else if (age >= 1 && age <= 9){
+      this.height = (age*25);
+    }else if (age>=10 && age <= 20){
+      this.height = (225+(age-9)*10);
+    }else if (age > 20){
+      this.height = 335;
+    }else{
+       "Not a valid age";
+    }
+  }
+
+
+  seed (age: number, height: number, alive: boolean): void  {
+    this.age = 0;
+    this.height = 0;
+    this.alive = true;
+  }
+}
+
+
 // Leave the line below for tests to work properly.
 export { Tree };

b62f11aafd2d68e6d3b51a46166a037bfe0eba7b
Baptiste Verkindere <bverkindere@gmail.com>
2022-02-02 10:40:24 +0100

    adding answers to exercise 2

--- 02_programming/17_typescript_generics/607d4b538285006c8eafd921b31409725dd8252e/02_object_transformer/src/index.ts
+++ 02_programming/17_typescript_generics/607d4b538285006c8eafd921b31409725dd8252e/02_object_transformer/src/index.ts
@@ -1,3 +1,20 @@
+type Product = {
+  productName: string;
+  quantity: number;
+}
+
+const myProduct: Product = {
+  productName: "phone",
+  quantity: 10
+}
+
+function productTransformer (object: Product): [string, string | number ][] {
+  console.log(Object.entries(object));
+  return Object.entries(object)
+  // console.log(Object.keys(object).map((key) => [Number(key), object[key]]))
+}
+
+productTransformer(myProduct);
 
 // Leave the line below for tests to work properly.
 export { productTransformer };

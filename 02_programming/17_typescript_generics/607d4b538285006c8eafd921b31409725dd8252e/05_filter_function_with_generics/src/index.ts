92442e453ec032a976f19dd46f0b740fb03af79d
Baptiste Verkindere <bverkindere@gmail.com>
2022-02-02 12:18:41 +0100

    adding aswers to exercise 5

--- 02_programming/17_typescript_generics/607d4b538285006c8eafd921b31409725dd8252e/05_filter_function_with_generics/src/index.ts
+++ 02_programming/17_typescript_generics/607d4b538285006c8eafd921b31409725dd8252e/05_filter_function_with_generics/src/index.ts
@@ -1,5 +1,5 @@
-function filterFor(array, predicate) {
-  const resultArray = [];
+function filterFor<T>(array: T[], predicate:(element: T) => boolean): T[] {
+    const resultArray = [];
 
   for (const element of array) {
     if (predicate(element)) {

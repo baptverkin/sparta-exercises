ca0f4694ced4c3ef556a8e1701872c59befdc621
Baptiste Verkindere <bverkindere@gmail.com>
2022-02-02 16:14:48 +0100

    adding answer to orange tree bonus exercise

--- 02_programming/16_typescript_oop/bf7decead6a2841402b38470fef1130c0f7a2ef3/04_orange_tree_part_three_bonus/src/OrangeTree.ts
+++ 02_programming/16_typescript_oop/bf7decead6a2841402b38470fef1130c0f7a2ef3/04_orange_tree_part_three_bonus/src/OrangeTree.ts
@@ -2,5 +2,92 @@ import { Tree } from "./Tree";
 
 // Paste your previous `OrangeTree` class here.
 
+class OrangeTree extends Tree {
+  protected _oranges: string[] = [];
+
+  constructor (age:number) {
+    super(age);
+    this._isAlive();
+    this.ageOneYear();
+    this.growOranges();
+  }
+
+  get oranges(): string[]{
+    return this._oranges;
+  }
+  get age(): number{
+    return this._age;
+  }
+  get height(): number{
+    return this._height;
+  }
+  get alive(): boolean{
+    return this._alive;
+  }
+
+  growOranges(): void {
+    const orangeEmoji: string = "🍊";
+    this._oranges.splice(0,this._oranges.length);
+
+    if (this._age>=0 && this._age <=4){
+       this._oranges;
+    } else if (this._age>=5 && this._age <=10 ){
+      for (let i:number = 1; i<=10; i++){
+        this._oranges.push(orangeEmoji)
+      }
+    } else if (this._age >=11 && this._age <=20 ){
+      for (let i:number = 1; i<=20; i++){
+        this._oranges.push(orangeEmoji)
+      }
+    } else if (this._age>=21 && this._age <=40 ){
+      for (let i:number = 1; i<=5; i++){
+        this._oranges.push(orangeEmoji.repeat(5))
+      }
+    } else {
+      this._oranges
+    }
+
+  }
+
+  protected _isAlive(): boolean {
+    const randomBoolean: boolean = Math.random() < (1-(this._age/100));
+    if (this._age >=0 && this._age <=50){
+      return true;
+    }else if (this._age >50 && this._age <= 99){
+      return randomBoolean;
+    }else {
+      return false;
+    }
+}
+
+
+  ageOneYear (): void {
+    this._age += 1;
+
+    if (this._age <= 9) {
+      this._height += 25;
+      this.growOranges();
+    } else if (this._age >= 10 && this._age <= 20) {
+      this._height += 10;
+      this.growOranges();
+    }else if (this._age >20){
+      this.growOranges();
+    }
+    this._alive = this._isAlive();
+    this.growOranges();
+  }
+
+    pickAnOrange(): string[] {
+      if (this._oranges !== []){
+      return this._oranges.splice(0,1);
+      }else{
+        return [];
+      }
+    }
+
+}
+
+
+
 // Leave the line below for tests to work properly.
 export { OrangeTree };
--- 02_programming/16_typescript_oop/bf7decead6a2841402b38470fef1130c0f7a2ef3/04_orange_tree_part_three_bonus/src/Tree.ts
+++ 02_programming/16_typescript_oop/bf7decead6a2841402b38470fef1130c0f7a2ef3/04_orange_tree_part_three_bonus/src/Tree.ts
@@ -1,4 +1,33 @@
 // Paste your previous `Tree` class here.
+abstract class Tree {
+  protected _age: number;
+  protected _height: number;
+  protected _alive: boolean = true;
 
+  constructor (age: number){
+    this._age = age;
+    if (age===0){
+      this._height = 0;
+    }else if (age >= 1 && age <= 9){
+      this._height = (age*25);
+    }else if (age>=10 && age <= 20){
+      this._height = (225+(age-9)*10);
+    }else if (age > 20){
+      this._height = 335;
+    }else{
+       "Not a valid age";
+    }
+  }
+
+  protected abstract _isAlive(): boolean;
+  protected abstract ageOneYear(): void;
+
+
+  seed (age: number, height: number, alive: boolean): void  {
+    this._age = 0;
+    this._height = 0;
+    this._alive = true;
+  }
+}
 // Leave the line below for tests to work properly.
 export { Tree };

920783793b84aca7efa33fd02a3a17547ba36f55
Baptiste Verkindere <bverkindere@gmail.com>
2022-02-08 11:04:17 +0100

    adding answers to exercise 2

--- 03_frontend_development/02_express/f5c04629aa46be23ca8de8a06fba33f447289dc8/02_warmup_sorting_machine/src/index.ts
+++ 03_frontend_development/02_express/f5c04629aa46be23ca8de8a06fba33f447289dc8/02_warmup_sorting_machine/src/index.ts
@@ -1,1 +1,13 @@
 // Code here
+// function coompare (a: number, b: number)
+
+
+function sortingMachine (revenue: number[], order: boolean): number[]{
+  if (order === true){
+    return revenue.sort(function(a, b){return a-b});
+  }else if(order ===false){
+    return revenue.sort(function(a, b){return b-a});
+  }
+}
+
+export {sortingMachine}

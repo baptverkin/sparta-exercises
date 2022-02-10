f2d2bab3c134c4b211176025d321b44914073229
Baptiste Verkindere <bverkindere@gmail.com>
2022-02-10 12:17:55 +0100

    adding answers to exercise 2

--- 03_frontend_development/04_forms_and_cookies/e5b3d8438cc5eff9e68b205b3efb73619e89654c/02_fundamentals_antiVowels_context/src/context.ts
+++ 03_frontend_development/04_forms_and_cookies/e5b3d8438cc5eff9e68b205b3efb73619e89654c/02_fundamentals_antiVowels_context/src/context.ts
@@ -2,6 +2,10 @@ import { sentenceJoiner } from './sentenceJoiner';
 import { sentenceSplitter } from './sentenceSplitter';
 
 
-export const context = () => {
-  // Code your function for context here
+export const context = (sentence: string): string => {
+  const arr = sentenceSplitter(sentence).filter((e: string):boolean =>
+  e.charAt(0) !== "a" && e.charAt(0) !== "e" && e.charAt(0) !== "i" && e.charAt(0) !== "o" && e.charAt(0) !== "u" && e.charAt(0) !== "y" && e.charAt(0)  !== "A" && e.charAt(0) !== "E" && e.charAt(0) !== "I" && e.charAt(0) !== "O" && e.charAt(0) !== "U" && e.charAt(0) !== "Y")
+  console.log(arr);
+  return sentenceJoiner(arr);
 };
+
--- 03_frontend_development/04_forms_and_cookies/e5b3d8438cc5eff9e68b205b3efb73619e89654c/02_fundamentals_antiVowels_context/src/sentenceJoiner.ts
+++ 03_frontend_development/04_forms_and_cookies/e5b3d8438cc5eff9e68b205b3efb73619e89654c/02_fundamentals_antiVowels_context/src/sentenceJoiner.ts
@@ -1,1 +1,4 @@
 // Copy your function `sentenceJoiner` from the first exercise here
+export const sentenceJoiner = (arr: string[]): string => {
+  return arr.join(" ");
+};

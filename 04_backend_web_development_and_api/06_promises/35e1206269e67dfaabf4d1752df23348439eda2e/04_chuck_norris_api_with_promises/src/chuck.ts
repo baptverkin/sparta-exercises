524ccbea4a0261f8c304891e2834a6b37b91ec65
Baptiste Verkindere <bverkindere@gmail.com>
2022-02-21 12:00:27 +0100

    adding answers to exercise 4

--- 04_backend_web_development_and_api/06_promises/35e1206269e67dfaabf4d1752df23348439eda2e/04_chuck_norris_api_with_promises/src/chuck.ts
+++ 04_backend_web_development_and_api/06_promises/35e1206269e67dfaabf4d1752df23348439eda2e/04_chuck_norris_api_with_promises/src/chuck.ts
@@ -1,9 +1,18 @@
-function getCategories() {
-  // Your code goes here
+import fetch from "node-fetch";
+
+function getCategories(): Promise<string[]> {
+  return fetch("https://api.chucknorris.io/jokes/categories")
+    .then((response) => response.json())
+    .then((result) => result);
+  // .catch((error) => {
+  //   console.error(error);
+  // });
 }
 
-function getJoke() {
-  // Your code goes here
+function getJoke(category: string) {
+  return fetch(`https://api.chucknorris.io/jokes/random?category=${category}`)
+    .then((response) => response.json())
+    .then((result) => result.value);
 }
 
 // Leave the line below for tests to work properly

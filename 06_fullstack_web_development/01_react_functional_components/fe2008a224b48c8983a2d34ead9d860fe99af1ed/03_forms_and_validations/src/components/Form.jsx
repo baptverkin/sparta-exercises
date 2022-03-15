8936858d4ba0d5434ba8e81f14bd9e4379e3d394
Baptiste Verkindere <bverkindere@gmail.com>
2022-03-15 15:41:58 +0100

    adding answers to exercise 3

--- 06_fullstack_web_development/01_react_functional_components/fe2008a224b48c8983a2d34ead9d860fe99af1ed/03_forms_and_validations/src/components/Form.jsx
+++ 06_fullstack_web_development/01_react_functional_components/fe2008a224b48c8983a2d34ead9d860fe99af1ed/03_forms_and_validations/src/components/Form.jsx
@@ -1,5 +1,37 @@
 import React from "react";
 
-const Form = () => "Code here";
+const Form = () => {
+  const [space, setSpace] = React.useState(false);
+
+  function validateInput(value) {
+    console.log(value);
+    value.includes(" ") ? setSpace(!space) : null;
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("You clicked submit.");
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label htmlFor="exampleInputEmail1">First Name</label>
+        <input
+          onChange={(e) => validateInput(e.target.value)}
+          type="text"
+          className="form-control"
+          id="exampleFirstName"
+        />
+        <small id="FirstName Help" className="form-text text-muted"></small>
+        {space ? "No Spaces Allowed" : null}
+      </div>
+
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
+};
 
 export default Form;
